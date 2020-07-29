107476a18957e49332f603232f92635191cda39d
mimu <mimu@Mac-mini.local>
2020-07-29 08:28:00 +0800

    update single-server2

--- server-test2/src/main/java/test/single/server/test2/controller/SkyWalkingTestController.java
+++ server-test2/src/main/java/test/single/server/test2/controller/SkyWalkingTestController.java
@@ -12,6 +12,9 @@ import test.single.server.test2.model.SchoolSchoolInfo;
 import test.single.server.test2.request.RpcRequest;
 import test.single.server.test2.service.SkyWalkingTestService;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
 @RestController
 public class SkyWalkingTestController {
 
@@ -38,9 +41,14 @@ public class SkyWalkingTestController {
     public String getRpc2Info(SimpleHttpRequest request, SimpleHttpResponse response) {
         try {
             RpcRequest convert = ConvertUtil.convert(request, RpcRequest.class);
-            String url = "http://localhost:9093/rpc3/info.do?serial=%s";
-            String s = SimpleHttpClient.get(String.format(url, convert.getSerial()));
-            return s;
+            Callable<String> callable = () -> {
+                String url = "http://localhost:9093/rpc3/info.do?serial=%s";
+                String s = SimpleHttpClient.get(String.format(url, convert.getSerial()));
+                return s;
+            };
+            FutureTask<String> stringFutureTask = new FutureTask<>(callable);
+            new Thread(stringFutureTask).start();
+            return stringFutureTask.get();
         } catch (Exception e) {
             return "";
         }
